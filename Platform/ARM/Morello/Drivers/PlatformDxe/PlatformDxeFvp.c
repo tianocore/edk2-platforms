fb66890137890b9381d420ed558115c28ec2dbb5
Patrik Berglund <patrik.berglund@arm.com>
2022-06-13 12:59:49 +0100

    Platform/ARM/Morello: Ensure GOP is enabled at runtime in FVP
    Because the FVP do not have any input drivers in UEFI we do not want to
    enable the console redirection on it.
    However, we want the GOP to be made available to the OS (that may have
    drivers).
    This is achieved by setting a mode if one has not already been set.
    
    Signed-off-by: Varshit Pandya <varshit.pandya@arm.com>
    Change-Id: Ie74240396b14083b82c174cd49c37ebadf6ba941

--- Platform/ARM/Morello/Drivers/PlatformDxe/PlatformDxeFvp.c
+++ Platform/ARM/Morello/Drivers/PlatformDxe/PlatformDxeFvp.c
@@ -1,13 +1,132 @@
 /** @file
 
-  Copyright (c) 2021, ARM Limited. All rights reserved.<BR>
+  Copyright (c) 2021 - 2022, ARM Limited. All rights reserved.<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
 #include <Library/DebugLib.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Protocol/GraphicsOutput.h>
 #include <Protocol/RamDisk.h>
+
+/// The DXE singleton data
+STATIC
+struct {
+  EFI_EVENT    GopEvent;
+  VOID         *GopNotifyRegistrationKey;
+} mModule;
+
+/**
+  Called when the first GOP is installed and set a mode.
+
+  @param[in] Event   Not used.
+  @param[in] Context Not used.
+**/
+STATIC
+VOID
+OnGopNotifyCallback (
+  IN EFI_EVENT  Event    OPTIONAL,
+  IN VOID       *Context OPTIONAL
+  )
+{
+  EFI_STATUS                    Status;
+  EFI_GRAPHICS_OUTPUT_PROTOCOL  *Gop;
+
+  Status = gBS->LocateProtocol (
+                  &gEfiGraphicsOutputProtocolGuid,
+                  mModule.GopNotifyRegistrationKey,
+                  (VOID **)&Gop
+                  );
+  if (EFI_ERROR (Status)) {
+    if (Status != EFI_NOT_FOUND) {
+      DEBUG ((
+        DEBUG_ERROR,
+        "[%a:%a]: LocateProtocol(gEfiGraphicsOutputProtocolGuid) - %r\n",
+        gEfiCallerBaseName,
+        __func__,
+        Status
+        ));
+    }
+
+    return;
+  }
+
+  // Make sure that a mode have been set so we can hand the OS a running GOP
+  Status = Gop->SetMode (Gop, 0);
+  if (EFI_ERROR (Status)) {
+    if (Status != EFI_NOT_FOUND) {
+      DEBUG ((
+        DEBUG_ERROR,
+        "[%a:%a]: SetMode() - %r\n",
+        gEfiCallerBaseName,
+        __func__,
+        Status
+        ));
+    }
+
+    return;
+  }
+
+  Status = gBS->CloseEvent (mModule.GopEvent);
+  ASSERT_EFI_ERROR (Status);
+  mModule.GopEvent                 = NULL;
+  mModule.GopNotifyRegistrationKey = NULL;
+}
+
+/** Initialize the GOP notification callback event.
+
+  @retval EFI_SUCCESS  The GOP notify callback was successfully installed.
+  @retval *            Errors are possible.
+
+**/
+STATIC
+EFI_STATUS
+InitGopNotify (
+  VOID
+  )
+{
+  EFI_STATUS  Status;
+
+  Status = gBS->CreateEvent (
+                  EVT_NOTIFY_SIGNAL,
+                  TPL_NOTIFY,
+                  OnGopNotifyCallback,
+                  NULL,
+                  &mModule.GopEvent
+                  );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "[%a]: Can not install the NotifyEvent handler -- %r\n",
+      gEfiCallerBaseName,
+      Status
+      ));
+    return Status;
+  }
+
+  Status = gBS->RegisterProtocolNotify (
+                  &gEfiGraphicsOutputProtocolGuid,
+                  mModule.GopEvent,
+                  &mModule.GopNotifyRegistrationKey
+                  );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "[%a]: RegisterProtocolNotify(gEfiGraphicsOutputProtocolGuid) -- %r\n",
+      gEfiCallerBaseName,
+      Status
+      ));
+    return Status;
+  }
+
+  /** Explicitly signal the GopEvent to trigger the invocation of SetMode(),
+      in case this module was loaded after the GOP was initialised
+  **/
+  Status = gBS->SignalEvent (mModule.GopEvent);
+  ASSERT_EFI_ERROR (Status);
+  return Status;
+}
 
 VOID
 InitVirtioDevices (
@@ -21,6 +140,7 @@ InitVirtioDevices (
   @param  SystemTable[in]       A pointer to the EFI System Table.
 
   @retval EFI_SUCCESS           The entry point is executed successfully.
+  @retval *                     Errors are possible.
 **/
 EFI_STATUS
 EFIAPI
@@ -33,7 +153,16 @@ ArmMorelloEntryPoint (
   EFI_RAM_DISK_PROTOCOL     *RamDisk;
   EFI_DEVICE_PATH_PROTOCOL  *DevicePath;
 
-  Status = EFI_SUCCESS;
+  Status = InitGopNotify ();
+  if (EFI_ERROR (Status)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "[%a]: Failed to initialize GOP notification."
+      " Can not gurantee that the runtime will have a GOP -- %r\n",
+      gEfiCallerBaseName,
+      Status
+      ));
+  }
 
   InitVirtioDevices ();
 
