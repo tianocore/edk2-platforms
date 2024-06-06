4fcecf412014398b7ce30d88f780f70045e6d8fa
Jiaxin Wu <jiaxin.wu@intel.com>
2024-06-06 15:02:26 +0800

    Platform/Intel: Consume SmmRelocationInit interface
    Do the smm relocation during platform init.
    
    Signed-off-by: Jiaxin Wu <jiaxin.wu@intel.com>

--- Platform/Intel/MinPlatformPkg/PlatformInit/PlatformInitPei/PlatformInitPostMem.c
+++ Platform/Intel/MinPlatformPkg/PlatformInit/PlatformInitPei/PlatformInitPostMem.c
@@ -1,7 +1,7 @@
 /** @file
   Source code file for Platform Init PEI module
 
-Copyright (c) 2017 - 2019, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2017 - 2024, Intel Corporation. All rights reserved.<BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -13,6 +13,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/PeiServicesLib.h>
 #include <IndustryStandard/Pci30.h>
 #include <Ppi/EndOfPeiPhase.h>
+#include <Ppi/MpServices2.h>
 
 #include <Guid/FirmwareFileSystem2.h>
 #include <Protocol/FirmwareVolumeBlock.h>
@@ -21,6 +22,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/BoardInitLib.h>
 #include <Library/TestPointCheckLib.h>
 #include <Library/SetCacheMtrrLib.h>
+#include <Library/SmmRelocationLib.h>
 
 EFI_STATUS
 EFIAPI
@@ -30,10 +32,41 @@ PlatformInitEndOfPei (
   IN VOID                       *Ppi
   );
 
+/**
+  Notification function called when EDKII_PEI_MP_SERVICES2_PPI becomes available.
+
+  @param[in] PeiServices      Indirect reference to the PEI Services Table.
+  @param[in] NotifyDescriptor Address of the notification descriptor data
+                              structure.
+  @param[in] Ppi              Address of the PPI that was installed.
+
+  @return  Status of the notification. The status code returned from this
+           function is ignored.
+**/
+STATIC
+EFI_STATUS
+EFIAPI
+OnMpServices2Available (
+  IN EFI_PEI_SERVICES           **PeiServices,
+  IN EFI_PEI_NOTIFY_DESCRIPTOR  *NotifyDescriptor,
+  IN VOID                       *Ppi
+  );
+
 static EFI_PEI_NOTIFY_DESCRIPTOR  mEndOfPeiNotifyList = {
   (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
   &gEfiEndOfPeiSignalPpiGuid,
   (EFI_PEIM_NOTIFY_ENTRY_POINT) PlatformInitEndOfPei
+};
+
+//
+// Notification object for registering the callback, for when
+// EFI_PEI_MP_SERVICES_PPI becomes available.
+//
+STATIC CONST EFI_PEI_NOTIFY_DESCRIPTOR  mMpServices2Notify = {
+  EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK |   // Flags
+  EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST,
+  &gEdkiiPeiMpServices2PpiGuid,              // Guid
+  OnMpServices2Available                     // Notify
 };
 
 /**
@@ -72,6 +105,41 @@ PlatformInitEndOfPei (
   return Status;
 }
 
+/**
+  Notification function called when EDKII_PEI_MP_SERVICES2_PPI becomes available.
+
+  @param[in] PeiServices      Indirect reference to the PEI Services Table.
+  @param[in] NotifyDescriptor Address of the notification descriptor data
+                              structure.
+  @param[in] Ppi              Address of the PPI that was installed.
+
+  @return  Status of the notification. The status code returned from this
+           function is ignored.
+**/
+STATIC
+EFI_STATUS
+EFIAPI
+OnMpServices2Available (
+  IN EFI_PEI_SERVICES           **PeiServices,
+  IN EFI_PEI_NOTIFY_DESCRIPTOR  *NotifyDescriptor,
+  IN VOID                       *Ppi
+  )
+{
+  EDKII_PEI_MP_SERVICES2_PPI  *MpServices2;
+  EFI_STATUS                  Status;
+
+  MpServices2 = Ppi;
+
+  //
+  // Smm Relocation Initialize.
+  //
+  Status = SmmRelocationInit (MpServices2);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_WARN, "OnMpServices2Available: Not able to execute Smm Relocation Init.  Status: %r\n", Status));
+  }
+
+  return EFI_SUCCESS;
+}
 
 /**
   Platform Init PEI module entry point
@@ -92,6 +160,9 @@ PlatformInitPostMemEntryPoint (
   EFI_STATUS                       Status;
 
   Status = BoardInitBeforeSiliconInit ();
+  ASSERT_EFI_ERROR (Status);
+
+  Status = PeiServicesNotifyPpi (&mMpServices2Notify);
   ASSERT_EFI_ERROR (Status);
 
   //
