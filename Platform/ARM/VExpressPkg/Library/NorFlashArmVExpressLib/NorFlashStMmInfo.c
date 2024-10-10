76478fd4f409f91721f6df0aaeb848eedae99ae9
levi.yun <yeoreum.yun@arm.com>
2024-10-10 11:37:45 +0100

    Platform/ARM/VExpressPkg/NorFlashArmVExpressLib: add TPM storage information
    add TPM NV storage information in StandaloneMm.
    
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>

--- Platform/ARM/VExpressPkg/Library/NorFlashArmVExpressLib/NorFlashStMmInfo.c
+++ Platform/ARM/VExpressPkg/Library/NorFlashArmVExpressLib/NorFlashStMmInfo.c
@@ -10,6 +10,24 @@
 #include <Library/NorFlashPlatformLib.h>
 #include <ArmPlatform.h>
 
+/*
+ * This is secure world's view of the nor flash 1 area (secure only).
+ *
+ * +--------------------------------+ 0x10000000
+ * |    Variable Storage (256KB)    | (ARM_VE_SMB_NOR1_BASE + ARM_VE_SMB_NOR1_SZ)
+ * |  (ENABLE_UEFI_SECURE_VARAIBLE) |
+ * +--------------------------------+ 0x0ffc0000
+ * |     TPM Nv Storage (256KB)     |
+ * |          (ENABLE_TPM)          |
+ * +--------------------------------+ 0x0ff80000
+ * |                                |
+ * |                                |
+ * |     Reserved  (63MB - 512KB)   |
+ * |                                |
+ * |                                |
+ * +--------------------------------+ 0x0c000000
+ *                                    (ARM_VE_SMB_NOR1_BASE)
+ */
 NOR_FLASH_DESCRIPTION mNorFlashDevices[] = {
 #ifdef ENABLE_FIRMWARE_UPDATE
   { // FWU
@@ -23,8 +41,14 @@ NOR_FLASH_DESCRIPTION mNorFlashDevices[] = {
   {
     ARM_VE_SMB_NOR1_BASE,
     ARM_VE_SMB_NOR1_BASE,
-    SIZE_256KB * 255,
+    SIZE_256KB * 254,
     SIZE_256KB,
+  },
+  { // Tpm Nv area. This should be over PcdTpmNvMemoryBaseSize + 0x200 (metadata block).
+    ARM_VE_SMB_NOR1_BASE,
+    ARM_VE_SMB_NOR1_BASE + SIZE_256KB * 254,
+    SIZE_256KB,
+    SIZE_64KB,
   },
   { // UEFI Variable Services non-volatile storage
     ARM_VE_SMB_NOR1_BASE,
