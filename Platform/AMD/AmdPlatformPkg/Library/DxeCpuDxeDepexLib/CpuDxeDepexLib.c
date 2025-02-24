75e0d3d48da3a30b564c3c38237354df68b78d44
cchen105 <Chen.Chen@amd.com>
2025-02-24 08:59:41 +0800

    AmdPlatformPkg: Add DxeCpuDxeDepex Library.

---
+++ Platform/AMD/AmdPlatformPkg/Library/DxeCpuDxeDepexLib/CpuDxeDepexLib.c
@@ -0,0 +1,20 @@
+/** @file
+  CpuDxe Dependency Lib.
+
+  Copyright (C) 2017 - 2025, Advanced Micro Devices, Inc. All rights reserved.<BR>
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Base.h>
+
+/**
+  Empty function to allow forcing a Depex on CpuDxe Driver
+**/
+VOID
+CpuDxeDepexPlaceHolderFunction (
+  VOID
+  )
+{
+}
