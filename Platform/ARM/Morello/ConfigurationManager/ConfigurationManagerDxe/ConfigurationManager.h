3ba6aae6eae7da7e1fcc82e2e83b7b1cfa961790
Varshit Pandya <varshit.pandya@arm.com>
2025-06-03 16:07:13 +0100

    Platform/ARM/Morello: Fix formatting
    Format all files with uncrustify
    
    Signed-off-by: Varshit Pandya <varshit.pandya@arm.com>

--- Platform/ARM/Morello/ConfigurationManager/ConfigurationManagerDxe/ConfigurationManager.h
+++ Platform/ARM/Morello/ConfigurationManager/ConfigurationManagerDxe/ConfigurationManager.h
@@ -14,7 +14,7 @@
 
 /** The configuration manager version.
 */
-#define CONFIGURATION_MANAGER_REVISION CREATE_REVISION (1, 0)
+#define CONFIGURATION_MANAGER_REVISION  CREATE_REVISION (1, 0)
 
 /** The OEM ID
 */
@@ -23,8 +23,8 @@
 
 /** A helper macro for mapping a reference token
 */
-#define REFERENCE_TOKEN(Field)                                    \
-  (CM_OBJECT_TOKEN)((UINT8*)&CommonPlatformInfo +                 \
+#define REFERENCE_TOKEN(Field)                                \
+  (CM_OBJECT_TOKEN)((UINT8*)&CommonPlatformInfo +             \
     OFFSET_OF (EDKII_COMMON_PLATFORM_REPOSITORY_INFO, Field))
 
 /** A helper macro that constructs the MPID based on the
@@ -35,67 +35,59 @@
 
 /** A helper macro for populating the GIC CPU information
 */
-#define GICC_ENTRY(                                                      \
-          CPUInterfaceNumber,                                            \
-          Mpidr,                                                         \
-          PmuIrq,                                                        \
-          VGicIrq,                                                       \
-          GicRedistBase,                                                 \
-          EnergyEfficiency,                                              \
-          SpeIrq,                                                        \
-          ProximityDomain,                                               \
-          ClockDomain                                                    \
-          ) {                                                            \
-    CPUInterfaceNumber,       /* UINT32  CPUInterfaceNumber           */ \
-    CPUInterfaceNumber,       /* UINT32  AcpiProcessorUid             */ \
-    EFI_ACPI_6_2_GIC_ENABLED, /* UINT32  Flags                        */ \
-    0,                        /* UINT32  ParkingProtocolVersion       */ \
-    PmuIrq,                   /* UINT32  PerformanceInterruptGsiv     */ \
-    0,                        /* UINT64  ParkedAddress                */ \
-    FixedPcdGet64 (                                                      \
-      PcdGicInterruptInterfaceBase                                       \
-      ),                      /* UINT64  PhysicalBaseAddress          */ \
-    0,                        /* UINT64  GICV                         */ \
-    0,                        /* UINT64  GICH                         */ \
-    VGicIrq,                  /* UINT32  VGICMaintenanceInterrupt     */ \
-    GicRedistBase,            /* UINT64  GICRBaseAddress              */ \
-    Mpidr,                    /* UINT64  MPIDR                        */ \
-    EnergyEfficiency,         /* UINT8   ProcessorPowerEfficiencyClass*/ \
-    SpeIrq,                   /* UINT16  SpeOverflowInterrupt         */ \
-    ProximityDomain,          /* UINT32  ProximityDomain              */ \
-    ClockDomain,              /* UINT32  ClockDomain                  */ \
-    EFI_ACPI_6_3_GICC_ENABLED,/* UINT32  Flags                        */ \
-    }
+#define GICC_ENTRY(CPUInterfaceNumber, \
+                   Mpidr,              \
+                   PmuIrq,             \
+                   VGicIrq,            \
+                   GicRedistBase,      \
+                   EnergyEfficiency,   \
+                   SpeIrq,             \
+                   ProximityDomain,    \
+                   ClockDomain         \
+                   )  { CPUInterfaceNumber,                           /* UINT32  CPUInterfaceNumber           */ \
+                        CPUInterfaceNumber,                           /* UINT32  AcpiProcessorUid             */ \
+                        EFI_ACPI_6_2_GIC_ENABLED,                     /* UINT32  Flags                        */ \
+                        0,                                            /* UINT32  ParkingProtocolVersion       */ \
+                        PmuIrq,                                       /* UINT32  PerformanceInterruptGsiv     */ \
+                        0,                                            /* UINT64  ParkedAddress                */ \
+                        FixedPcdGet64 (PcdGicInterruptInterfaceBase), /* UINT64  PhysicalBaseAddress          */ \
+                        0,                                            /* UINT64  GICV                         */ \
+                        0,                                            /* UINT64  GICH                         */ \
+                        VGicIrq,                                      /* UINT32  VGICMaintenanceInterrupt     */ \
+                        GicRedistBase,                                /* UINT64  GICRBaseAddress              */ \
+                        Mpidr,                                        /* UINT64  MPIDR                        */ \
+                        EnergyEfficiency,                             /* UINT8   ProcessorPowerEfficiencyClass*/ \
+                        SpeIrq,                                       /* UINT16  SpeOverflowInterrupt         */ \
+                        ProximityDomain,                              /* UINT32  ProximityDomain              */ \
+                        ClockDomain,                                  /* UINT32  ClockDomain                  */ \
+                        EFI_ACPI_6_3_GICC_ENABLED,                    /* UINT32  Flags                        */ \
+                       }
 
 /** A helper macro for populating the Processor Hierarchy Node flags
 */
-#define PROC_NODE_FLAGS(                                                \
-          PhysicalPackage,                                              \
-          AcpiProcessorIdValid,                                         \
-          ProcessorIsThread,                                            \
-          NodeIsLeaf,                                                   \
-          IdenticalImplementation                                       \
-          )                                                             \
-  (                                                                     \
-    PhysicalPackage |                                                   \
-    (AcpiProcessorIdValid << 1) |                                       \
-    (ProcessorIsThread << 2) |                                          \
-    (NodeIsLeaf << 3) |                                                 \
-    (IdenticalImplementation << 4)                                      \
-  )
+#define PROC_NODE_FLAGS(PhysicalPackage,                \
+                        AcpiProcessorIdValid,           \
+                        ProcessorIsThread,              \
+                        NodeIsLeaf,                     \
+                        IdenticalImplementation         \
+                        )                               \
+                        (PhysicalPackage |              \
+                         (AcpiProcessorIdValid << 1) |  \
+                         (ProcessorIsThread << 2) |     \
+                         (NodeIsLeaf << 3) |            \
+                         (IdenticalImplementation << 4) \
+                        )
 
 /** A helper macro for populating the Cache Type Structure's attributes
 */
-#define CACHE_ATTRIBUTES(                                               \
-          AllocationType,                                               \
-          CacheType,                                                    \
-          WritePolicy                                                   \
-          )                                                             \
-  (                                                                     \
-    AllocationType |                                                    \
-    (CacheType << 2) |                                                  \
-    (WritePolicy << 4)                                                  \
-  )
+#define CACHE_ATTRIBUTES(AllocationType,     \
+                         CacheType,          \
+                         WritePolicy         \
+                         )                   \
+                         (AllocationType |   \
+                          (CacheType << 2) | \
+                          (WritePolicy << 4) \
+                         )
 
 /** A function that prepares Configuration Manager Objects for returning.
 
@@ -110,10 +102,10 @@
   @retval EFI_NOT_FOUND         The required object information is not found.
 **/
 typedef EFI_STATUS (*CM_OBJECT_HANDLER_PROC) (
-  IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  * CONST This,
+  IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  *CONST  This,
   IN  CONST CM_OBJECT_ID                                  CmObjectId,
   IN  CONST CM_OBJECT_TOKEN                               Token,
-  IN  OUT   CM_OBJ_DESCRIPTOR                     * CONST CmObject
+  IN  OUT   CM_OBJ_DESCRIPTOR                     *CONST  CmObject
   );
 
 /** A helper function for returning the Configuration Manager Objects.
@@ -131,10 +123,10 @@ EFI_STATUS
 EFIAPI
 HandleCmObject (
   IN  CONST CM_OBJECT_ID                CmObjectId,
-  IN        VOID                *       Object,
+  IN        VOID                        *Object,
   IN  CONST UINTN                       ObjectSize,
   IN  CONST UINTN                       ObjectCount,
-  IN  OUT   CM_OBJ_DESCRIPTOR   * CONST CmObjectDesc
+  IN  OUT   CM_OBJ_DESCRIPTOR   *CONST  CmObjectDesc
   );
 
 /** A helper function for returning the Configuration Manager Objects that
@@ -158,27 +150,27 @@ HandleCmObject (
 EFI_STATUS
 EFIAPI
 HandleCmObjectRefByToken (
-  IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  * CONST This,
+  IN  CONST EDKII_CONFIGURATION_MANAGER_PROTOCOL  *CONST  This,
   IN  CONST CM_OBJECT_ID                                  CmObjectId,
-  IN        VOID                                  *       Object,
+  IN        VOID                                          *Object,
   IN  CONST UINTN                                         ObjectSize,
   IN  CONST UINTN                                         ObjectCount,
   IN  CONST CM_OBJECT_TOKEN                               Token,
   IN  CONST CM_OBJECT_HANDLER_PROC                        HandlerProc,
-  IN  OUT   CM_OBJ_DESCRIPTOR                     * CONST CmObjectDesc
+  IN  OUT   CM_OBJ_DESCRIPTOR                     *CONST  CmObjectDesc
   );
 
 /** The number of CPUs
 */
-#define PLAT_CPU_COUNT              4
+#define PLAT_CPU_COUNT  4
 
 /** The number of platform generic timer blocks
 */
-#define PLAT_GTBLOCK_COUNT          1
+#define PLAT_GTBLOCK_COUNT  1
 
 /** The number of timer frames per generic timer block
 */
-#define PLAT_GTFRAME_COUNT          2
+#define PLAT_GTFRAME_COUNT  2
 
 /** The number of Processor Hierarchy Nodes
     - one package node
@@ -195,7 +187,7 @@ HandleCmObjectRefByToken (
     - core L2 cache
     - slc unified cache
 */
-#define PLAT_CACHE_COUNT                5
+#define PLAT_CACHE_COUNT  5
 
 /** The number of resources private to the cluster
     - L3 cache
@@ -218,63 +210,62 @@ HandleCmObjectRefByToken (
 */
 typedef struct CommonPlatformRepositoryInfo {
   /// Configuration Manager Information
-  CM_STD_OBJ_CONFIGURATION_MANAGER_INFO CmInfo;
+  CM_STD_OBJ_CONFIGURATION_MANAGER_INFO           CmInfo;
 
   /// Boot architecture information
-  CM_ARM_BOOT_ARCH_INFO                 BootArchInfo;
+  CM_ARM_BOOT_ARCH_INFO                           BootArchInfo;
 
-#ifdef HEADLESS_PLATFORM
+ #ifdef HEADLESS_PLATFORM
   /// Fixed feature flag information
-  CM_ARCH_COMMON_FIXED_FEATURE_FLAGS    FixedFeatureFlags;
-#endif
+  CM_ARCH_COMMON_FIXED_FEATURE_FLAGS              FixedFeatureFlags;
+ #endif
 
   /// Power management profile information
-  CM_ARCH_COMMON_POWER_MANAGEMENT_PROFILE_INFO  PmProfileInfo;
+  CM_ARCH_COMMON_POWER_MANAGEMENT_PROFILE_INFO    PmProfileInfo;
 
   /// GIC CPU interface information
-  CM_ARM_GICC_INFO                      GicCInfo[PLAT_CPU_COUNT];
+  CM_ARM_GICC_INFO                                GicCInfo[PLAT_CPU_COUNT];
 
   /// GIC distributor information
-  CM_ARM_GICD_INFO                      GicDInfo;
+  CM_ARM_GICD_INFO                                GicDInfo;
 
   /// GIC Redistributor information
-  CM_ARM_GIC_REDIST_INFO                GicRedistInfo;
+  CM_ARM_GIC_REDIST_INFO                          GicRedistInfo;
 
   /// Generic timer information
-  CM_ARM_GENERIC_TIMER_INFO             GenericTimerInfo;
+  CM_ARM_GENERIC_TIMER_INFO                       GenericTimerInfo;
 
   /// Generic timer block information
-  CM_ARM_GTBLOCK_INFO                   GTBlockInfo[PLAT_GTBLOCK_COUNT];
+  CM_ARM_GTBLOCK_INFO                             GTBlockInfo[PLAT_GTBLOCK_COUNT];
 
   /// Generic timer frame information
-  CM_ARM_GTBLOCK_TIMER_FRAME_INFO       GTBlock0TimerInfo[PLAT_GTFRAME_COUNT];
+  CM_ARM_GTBLOCK_TIMER_FRAME_INFO                 GTBlock0TimerInfo[PLAT_GTFRAME_COUNT];
 
   /// Watchdog information
-  CM_ARM_GENERIC_WATCHDOG_INFO          Watchdog;
+  CM_ARM_GENERIC_WATCHDOG_INFO                    Watchdog;
 
   /** Serial port information for the
       serial port console redirection port
   */
-  CM_ARCH_COMMON_SERIAL_PORT_INFO       SpcrSerialPort;
+  CM_ARCH_COMMON_SERIAL_PORT_INFO                 SpcrSerialPort;
 
   /// Serial port information for the DBG2 UART port
-  CM_ARCH_COMMON_SERIAL_PORT_INFO       DbgSerialPort;
+  CM_ARCH_COMMON_SERIAL_PORT_INFO                 DbgSerialPort;
 
   // Processor topology information
-  CM_ARCH_COMMON_PROC_HIERARCHY_INFO    ProcHierarchyInfo[PLAT_PROC_HIERARCHY_NODE_COUNT];
+  CM_ARCH_COMMON_PROC_HIERARCHY_INFO              ProcHierarchyInfo[PLAT_PROC_HIERARCHY_NODE_COUNT];
 
   // Cache information
-  CM_ARCH_COMMON_CACHE_INFO             CacheInfo[PLAT_CACHE_COUNT];
+  CM_ARCH_COMMON_CACHE_INFO                       CacheInfo[PLAT_CACHE_COUNT];
 
   // Cluster private resources
-  CM_ARCH_COMMON_OBJ_REF                ClusterResources[CLUSTER_RESOURCE_COUNT];
+  CM_ARCH_COMMON_OBJ_REF                          ClusterResources[CLUSTER_RESOURCE_COUNT];
 
   // Core private resources
-  CM_ARCH_COMMON_OBJ_REF                CoreResources[CORE_RESOURCE_COUNT];
+  CM_ARCH_COMMON_OBJ_REF                          CoreResources[CORE_RESOURCE_COUNT];
 
   // SoC Resources
-  CM_ARCH_COMMON_OBJ_REF                SocResources[SOC_RESOURCE_COUNT];
-
+  CM_ARCH_COMMON_OBJ_REF                          SocResources[SOC_RESOURCE_COUNT];
 } EDKII_COMMON_PLATFORM_REPOSITORY_INFO;
 
 #endif // CONFIGURATION_MANAGER_H_
