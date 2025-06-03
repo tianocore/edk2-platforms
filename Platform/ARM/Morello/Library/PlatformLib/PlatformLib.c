1d9bec1be5ae0a73f5ba46456bafddb655bcb77e
Varshit Pandya <varshit.pandya@arm.com>
2025-06-03 16:07:13 +0100

    Platform/ARM/Morello: Fix formatting
    Format all files with uncrustify
    
    Signed-off-by: Varshit Pandya <varshit.pandya@arm.com>

--- Platform/ARM/Morello/Library/PlatformLib/PlatformLib.c
+++ Platform/ARM/Morello/Library/PlatformLib/PlatformLib.c
@@ -9,7 +9,7 @@
 #include <Library/BaseLib.h>
 #include <Ppi/ArmMpCoreInfo.h>
 
-STATIC ARM_CORE_INFO mCoreInfoTable[] = {
+STATIC ARM_CORE_INFO  mCoreInfoTable[] = {
   { 0x0, 0x0 }, // Cluster 0, Core 0
   { 0x0, 0x1 }, // Cluster 0, Core 1
   { 0x1, 0x0 }, // Cluster 1, Core 0
@@ -41,7 +41,7 @@ ArmPlatformGetBootMode (
 **/
 RETURN_STATUS
 ArmPlatformInitialize (
-  IN  UINTN                     MpId
+  IN  UINTN  MpId
   )
 {
   return RETURN_SUCCESS;
@@ -58,20 +58,20 @@ ArmPlatformInitialize (
 **/
 EFI_STATUS
 PrePeiCoreGetMpCoreInfo (
-  OUT UINTN                   *CoreCount,
-  OUT ARM_CORE_INFO           **ArmCoreTable
+  OUT UINTN          *CoreCount,
+  OUT ARM_CORE_INFO  **ArmCoreTable
   )
 {
-  *CoreCount = sizeof (mCoreInfoTable) / sizeof (ARM_CORE_INFO);
+  *CoreCount    = sizeof (mCoreInfoTable) / sizeof (ARM_CORE_INFO);
   *ArmCoreTable = mCoreInfoTable;
   return EFI_SUCCESS;
 }
 
-STATIC ARM_MP_CORE_INFO_PPI mMpCoreInfoPpi = {
+STATIC ARM_MP_CORE_INFO_PPI  mMpCoreInfoPpi = {
   PrePeiCoreGetMpCoreInfo
 };
 
-STATIC EFI_PEI_PPI_DESCRIPTOR gPlatformPpiTable[] = {
+STATIC EFI_PEI_PPI_DESCRIPTOR  gPlatformPpiTable[] = {
   {
     EFI_PEI_PPI_DESCRIPTOR_PPI,
     &gArmMpCoreInfoPpiGuid,
@@ -95,5 +95,5 @@ ArmPlatformGetPlatformPpiList (
   )
 {
   *PpiListSize = sizeof (gPlatformPpiTable);
-  *PpiList = gPlatformPpiTable;
+  *PpiList     = gPlatformPpiTable;
 }
