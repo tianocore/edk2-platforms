1336f9efdd6f8b82f6dbcbcd8d4a6892df883bcb
levi.yun <yeoreum.yun@arm.com>
2025-10-24 15:33:00 +0100

    Platform/ARM/VExpressPkg: generate SRAT acpi table
    Generate SRAT table.
    
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>

--- Platform/ARM/VExpressPkg/ConfigurationManager/ConfigurationManagerDxe/ConfigurationManager.h
+++ Platform/ARM/VExpressPkg/ConfigurationManager/ConfigurationManagerDxe/ConfigurationManager.h
@@ -118,7 +118,7 @@ typedef EFI_STATUS (*CM_OBJECT_HANDLER_PROC) (
 
 /** The number of ACPI tables to install
 */
-#define PLAT_ACPI_TABLE_COUNT       11
+#define PLAT_ACPI_TABLE_COUNT       12
 
 #define PLAT_SMBIOS_TABLE_COUNT     2
 
