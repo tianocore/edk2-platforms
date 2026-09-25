03019366a1a19318965e1a6ae1e63b7d2dc7734b
Abdul Lateef Attar <AbdulLateef.Attar@amd.com>
2026-09-25 06:09:22 +0000

    AmdPlatformPkg: Update SmmCoreAmdSpiHcHookLib
    - Drop the unused BaseLib include from SmmCoreAmdSpiHcHookLib.c.
    - Use #pragma once in the header instead of an include guard.
    - List SmmCoreAmdSpiHcHookLib.h in the INF [Sources] section.
    - Keep the copyright and SPDX lines in the Venice header layout.
    
    Signed-off-by: Abdul Lateef Attar <AbdulLateef.Attar@amd.com>

--- Platform/AMD/AmdPlatformPkg/Library/SmmCoreAmdSpiHcHookLib/SmmCoreAmdSpiHcHookLib.h
+++ Platform/AMD/AmdPlatformPkg/Library/SmmCoreAmdSpiHcHookLib/SmmCoreAmdSpiHcHookLib.h
@@ -1,13 +1,12 @@
 /** @file
   Header file of SMM core platform hook for AMD SPI Host Controller state library
 
-  Copyright (C) 2023-2025 Advanced Micro Devices, Inc. All rights reserved.
-  SPDX-License-Identifier: BSD-2-Clause-Patent
+  Copyright (C) 2023 - 2025 Advanced Micro Devices, Inc. All rights reserved.
 
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
-#ifndef SMM_CORE_HOOK_AMD_SPI_HC_H_
-#define SMM_CORE_HOOK_AMD_SPI_HC_H_
+#pragma once
 
 ///
 /// Structure of AMD SPI HC State record
@@ -16,5 +15,3 @@ typedef struct {
   VOID          *SmmSpiHcStateInterface; ///< AMD SMM SPI HC State Protocol Interface
   EFI_HANDLE    SmmSpiHcStateHandle;     ///< Handle of MD SMM SPI HC State Protocol handle
 } SMM_CORE_HOOK_AMD_SPI_HC_STATE_CONTEXT;
-
-#endif // SMM_CORE_HOOK_AMD_SPI_HC_H_
